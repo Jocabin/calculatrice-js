151ea65e3665e04eea68442a19dd52ae3b582a93
Jocabin <kobyfar@hotmail.com>
2023-02-13 20:20:08 +0100

    Change calculator system from eval to class

--- Calculator.js
+++ Calculator.js
@@ -1,13 +1,34 @@
 export default class Calculator {
-    firstMember = 0
-    secondMember = 0
-    result = 0
-
-    constructor() {
-    }
+    firstMember = ''
+    secondMember = ''
+    result = ''
+    operation = null
+    isCalculComplete = false
 
     get result() {
         return this.result
+    }
+
+    get isCalculComplete() {
+        return this.isCalculComplete
+    }
+
+    set isCalculComplete(expression) {
+        this.isCalculComplete = expression
+    }
+
+    set operation(expression) {
+        this.operation = expression
+    }
+
+    setOperation(expression) {
+        this.operation = expression
+        this.isCalculComplete = false
+        this.result = ''
+    }
+
+    set result(expression) {
+        this.result = expression
     }
 
     set firstMember(expression) {
@@ -18,27 +39,49 @@ export default class Calculator {
         this.secondMember = expression
     }
 
-    sum() {
-        this.result = parseFloat(this.firstMember) + parseFloat(this.secondMember)
-    }
+    compute() {
+        switch (this.operation) {
+            case '+':
+                this.result = (parseFloat(this.firstMember) + parseFloat(this.secondMember)).toString()
+                break
+            case '-':
+                this.result = (parseFloat(this.firstMember) - parseFloat(this.secondMember)).toString()
+                break
+            case '*':
+                this.result = (parseFloat(this.firstMember) * parseFloat(this.secondMember)).toString()
+                break
+            case '/':
+                this.result = (parseFloat(this.firstMember) / parseFloat(this.secondMember)).toString()
+                break
+            case '%':
+                this.result = (parseFloat(this.firstMember) % parseFloat(this.secondMember)).toString()
+                break
+            default:
+                alert('opération inconnue')
+                break
+        }
 
-    substract() {
-        this.result = parseFloat(this.firstMember) - parseFloat(this.secondMember)
-    }
-
-    multiply() {
-        this.result = parseFloat(this.firstMember) * parseFloat(this.secondMember)
-    }
-
-    divide() {
-        this.result = parseFloat(this.firstMember) / parseFloat(this.secondMember)
-    }
-
-    modulo() {
-        this.result = parseFloat(this.firstMember) % parseFloat(this.secondMember)
+        this.operation = null
+        this.firstMember = this.result.toString()
+        this.secondMember = ''
+        this.isCalculComplete = true
     }
 
     reset() {
-        this.result = 0
+        this.result = ''
+        this.operation = null
+        this.firstMember = ''
+        this.secondMember = ''
+        this.isCalculComplete = false
+    }
+
+    deleteLastChar() {
+        if (!this.operation) {
+            this.firstMember = this.firstMember.substring(0, this.firstMember.length - 1)
+        } else if (this.operation && this.secondMember.length > 0) {
+            this.secondMember = this.firstMember.substring(0, this.firstMember.length - 1)
+        } else {
+            this.operation = null
+        }
     }
 }
--- main.js
+++ main.js
@@ -1,7 +1,79 @@
 import Calculator from './Calculator'
 
 const calculator = new Calculator()
+const themeSelector = document.querySelector('#themeToggle')
+const resetBtn = document.querySelector('#resetOp')
+const moduloBtn = document.querySelector('#moduloOp')
+const divideBtn = document.querySelector('#divideOp')
+const multiplyBtn = document.querySelector('#multiplyOp')
+const substractBtn = document.querySelector('#substractOp')
+const sumBtn = document.querySelector('#sumOp')
+const delLastBtn = document.querySelector('#delLastOp')
+const resultBtn = document.querySelector('#resultOp')
+const resultText = document.querySelector('.result')
+const numbersBtns = document.querySelectorAll('.keyboard button:not([id])')
+
+numbersBtns.forEach(el => el.addEventListener('click', (e) => {
+    if (calculator.isCalculComplete) {
+        calculator.reset()
+        updateScreen()
+    }
+
+    if (!calculator.operation) {
+        calculator.firstMember += e.target.value
+        resultText.textContent += e.target.value
+    } else {
+        calculator.secondMember += e.target.value
+        resultText.textContent += e.target.value
+    }
+}))
 
 function updateScreen() {
-
+    resultText.textContent = calculator.result
 }
+
+updateScreen()
+
+themeSelector.addEventListener('change', () => {
+    document.body.classList.toggle('dark')
+})
+
+resultBtn.addEventListener('click', () => {
+    calculator.compute()
+    updateScreen()
+})
+
+resetBtn.addEventListener('click', () => {
+    calculator.reset()
+    updateScreen()
+})
+
+delLastBtn.addEventListener('click', () => {
+    calculator.deleteLastChar()
+    updateScreen()
+})
+
+moduloBtn.addEventListener('click', () => {
+    calculator.setOperation('%')
+    resultText.textContent += calculator.operation
+})
+
+divideBtn.addEventListener('click', () => {
+    calculator.setOperation('/')
+    resultText.textContent += calculator.operation
+})
+
+multiplyBtn.addEventListener('click', () => {
+    calculator.setOperation('*')
+    resultText.textContent += calculator.operation
+})
+
+substractBtn.addEventListener('click', () => {
+    calculator.setOperation('-')
+    resultText.textContent += calculator.operation
+})
+
+sumBtn.addEventListener('click', () => {
+    calculator.setOperation('+')
+    resultText.textContent += calculator.operation
+})
